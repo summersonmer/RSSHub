fa7ab80ee12dd886e2b75f23d3bf601deb763128
summersonmer <42095162+summersonmer@users.noreply.github.com>
2018-08-19 01:02:47 +0800

    Update home.js

--- routes/yingjiesheng/home.js
+++ routes/yingjiesheng/home.js
@@ -11,3 +11,28 @@ module.exports = async (ctx) => {
             Referer: 'http://www.yingjiesheng.com',
         },
     });
+    
+    const data=response.date;
+    
+    const $ = cheerio.load(data);
+    const list = $('.note-list li');
+    
+    ctx.state.data = {
+        title: '应届生',
+        link: 'https://www.jianshu.com',
+        description: $('meta[name="description"]').attr('content'),
+        item:
+            list &&
+            list
+                .map((index, item) => {
+                    item = $(item);
+                    return {
+                        title: item.find('.title').text(),
+                        description: `作者：${item.find('.nickname').text()}<br>描述：${item.find('.abstract').text()}<br><img referrerpolicy="no-referrer" src="https:${item.find('.img-blur').data('echo')}">`,
+                        pubDate: new Date(item.find('.time').data('shared-at')).toUTCString(),
+                        link: `https://www.jianshu.com${item.find('.title').attr('href')}`,
+                    };
+                })
+                .get(),
+    };
+};
