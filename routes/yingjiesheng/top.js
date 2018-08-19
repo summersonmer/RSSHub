b713f68f4738cdf24ddf60f25b6b93907e3673c8
summersonmer <42095162+summersonmer@users.noreply.github.com>
2018-08-19 22:27:40 +0800

    Create top.js

---
+++ routes/yingjiesheng/top.js
@@ -0,0 +1,39 @@
+const axios = require('../../utils/axios');
+const cheerio = require('cheerio');
+const config = require('../../config');
+const iconv = require('iconv-lite');
+
+module.exports = async (ctx) => {
+    const response = await axios({
+        method: 'get',
+        url: 'http://www.yingjiesheng.com/commend-fulltime-1.html',
+        headers: {
+            'User-Agent': config.ua,
+        },
+        responseType: 'arraybuffer'
+    });
+
+    const data = iconv.decode(response.data,'gbk');
+
+    const $ = cheerio.load(data);
+    const list = $('.jobList tr');
+
+    ctx.state.data = {
+        title: '应届生求职网',
+        link: 'http://www.yingjiesheng.com/commend-fulltime-1.html',
+        description: $('meta[name="description"]').attr('content'),
+        item:
+            list &&
+            list
+                .map((index, item) => {
+                    item = $(item);
+                    return {
+                        title: item.find('a').text(),
+                        //description: `title：${item.find('a').text()}<br><img referrerpolicy="no-referrer" src="https:${item.find('.img-blur').data('echo')}">`,
+                        pubDate: new Date(item.find('td').text()).toUTCString(),
+                        link: `${item.find('a').attr('href')}`,
+                    };
+                })
+                .get(),
+    };
+};
